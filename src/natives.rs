7687483649c0597494ae3834cda26f31a9b60603
Sreyas <sreyassreelal@gmail.com>
2019-02-24 02:49:30 +0000

    return something

--- src/natives.rs
+++ src/natives.rs
@@ -1,10 +1,8 @@
-//use samp_sdk::types::Cell;
-//use samp_sdk::amx::{AmxResult, AMX};
+use samp_sdk::types::Cell;
+use samp_sdk::amx::{AmxResult, AMX};
 
 impl super::{{crate_name}}{
 	pub fn foo(&mut self,_:&AMX) -> AmxResult<Cell> {
-
+		Ok(1)
 	}
 }
-
-
